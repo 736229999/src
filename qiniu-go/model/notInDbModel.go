ed3827efc33e41f3a82a0f35c61d3e5f68ea7e35
fengzhiyuan <1015138659@qq.com>
2017-07-18 17:23:56 +0800

    de

--- qiniu-go/model/notInDbModel.go
+++
@@ -1,9 +0,0 @@
-package model
-//这里面的所有struct都不在数据库中，只是单独抽离出来
-type distributeRule struct {
-	level1 float32	//一级分销对应的分成百分比
-	level2 float32
-	level3 float32
-}
-
-
--- qiniu-go/util/const.go
+++
@@ -1,7 +0,0 @@
-package util
-//该文件存放一些常用的量，比如微信appid之类的
-
-type WxConfig struct {
-	Appid string
-	Appsecret string
-}
